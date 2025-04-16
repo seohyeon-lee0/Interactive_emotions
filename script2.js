ae2920c8e25cb7c5f152d4cee0197a6ec28d9a4c
seohyeon-lee0 <sunny1018003@gmail.com>
2025-04-16 09:18:09 -0500

    Delete script2.js

--- script2.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('.arrow-left').addEventListener('click', () => {
-	window.location.href = 'page1.html';
-});
-
-document.querySelector('.arrow-right').addEventListener('click', () => {
-	window.location.href = 'page3.html';
-});
