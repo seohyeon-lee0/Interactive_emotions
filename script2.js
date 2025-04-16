6b800e79b543a2bda55f2c92c04ebb0649668eca
seohyeon-lee0 <sunny1018003@gmail.com>
2025-04-16 23:18:49 +0900

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,7 @@
+document.querySelector('.arrow-left').addEventListener('click', () => {
+	window.location.href = 'index.html';
+});
+
+document.querySelector('.arrow-right').addEventListener('click', () => {
+	window.location.href = 'page3.html';
+});
