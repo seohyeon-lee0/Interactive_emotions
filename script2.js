7ec338fce378d65097715b75f8162bb2f2eb002d
seohyeon-lee0 <sunny1018003@gmail.com>
2025-04-16 20:31:36 +0900

    중간 프로젝트
    감각과 감정의 시각적•인터랙티브 표현

---
+++ script2.js
@@ -0,0 +1,7 @@
+document.querySelector('.arrow-left').addEventListener('click', () => {
+	window.location.href = 'page1.html';
+});
+
+document.querySelector('.arrow-right').addEventListener('click', () => {
+	window.location.href = 'page3.html';
+});
